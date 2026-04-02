b0701f2dc4dc856051bf5a4e0a6cae4d34409180
sharp541 <stonegate541@gmail.com>
2026-04-02 15:07:50 +0900

    nvim: add telescope.nvim

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      {
+        "<leader>ff",
+        function()
+          require("telescope.builtin").find_files()
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<leader>fg",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = "Live Grep",
+      },
+      {
+        "<leader>fb",
+        function()
+          require("telescope.builtin").buffers()
+        end,
+        desc = "Buffers",
+      },
+      {
+        "<leader>fh",
+        function()
+          require("telescope.builtin").help_tags()
+        end,
+        desc = "Help Tags",
+      },
+    },
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+          prompt_position = "top",
+        },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
+}
